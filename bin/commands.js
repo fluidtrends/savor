d199a7199e05e07d1ec98f4e0fd27659bdd75e7d
I. Dan Calinescu <idancalinescu@gmail.com>
2016-12-20 05:51:08 +0200

    fixed ci tools integrations

--- bin/commands.js
+++ bin/commands.js
@@ -2,16 +2,16 @@ var testBin = "./node_modules/.bin/mocha";
 var coverageBin = "./node_modules/.bin/istanbul";
 var lintBin = "./node_modules/.bin/eslint";
 
-var coverallsBin = "cat ./coverage/lcov.info | ./node_modules/.bin/coveralls";
-var codacyBin = "cat ./coverage/lcov.info | ./node_modules/.bin/codacy-coverage";
-var codeclimateBin = "cat ./coverage/lcov.info | ./node_modules/.bin/codeclimate-test-reporter";
+var coverallsBin = "sh";
+var codacyBin = "sh";
+var codeclimateBin = "sh";
 var rmBin = "rm";
 var testArgs = ['--recursive', 'test/specs/**/*.js'];
 var coverageLocalArgs = ['cover', './node_modules/.bin/_mocha', '--', 'test/specs/**/*.js'];
 var coverageArgs = ['cover', './node_modules/.bin/_mocha', '--report', 'lcovonly', '--', 'test/specs/**/*.js', '-R', 'spec'];
-var coverallsArgs = [];
-var codacyArgs = [];
-var codeclimateArgs = [];
+var coverallsArgs = ['-c', 'cat ./coverage/lcov.info | ./node_modules/.bin/coveralls'];
+var codacyArgs = ['-c', 'cat ./coverage/lcov.info | ./node_modules/.bin/codacy-coverage'];
+var codeclimateArgs = ['-c', './node_modules/.bin/codeclimate-test-reporter < ./coverage/lcov.info'];
 var removeCoverageArgs = ['-rf', './coverage'];
 var lintArgs = ['src/**/*.js', 'test/specs/**/*.js'];
 
@@ -44,14 +44,14 @@ var coveralls = [
 
 var codeclimate = [
     {
-        bin: coverageBin,
-        args: coverageArgs
-}, {
+        //         bin: coverageBin,
+        //         args: coverageArgs
+        // }, {
         bin: codeclimateBin,
         args: codeclimateArgs
-}, {
-        bin: rmBin,
-        args: removeCoverageArgs
+            // }, {
+            //         bin: rmBin,
+            //         args: removeCoverageArgs
 }]
 
 var codacy = [{
--- bin/index.js
+++ bin/index.js
@@ -1,31 +1,36 @@
 #!/usr/bin/env node
 
-var spawn     = require('child_process').spawnSync;
-var commands  = require('./commands');
-var fs        = require('fs-extra');
-var path      = require('path');
+var spawn = require('child_process').spawnSync;
+var commands = require('./commands');
+var fs = require('fs-extra');
+var path = require('path');
 
-var resources = { lint: ['.eslintrc'] };
+var resources = {
+    lint: ['.eslintrc']
+};
 
-var cmdName   = 'test';
-var command   = commands.test;
-var cwd       = process.cwd();
-var resDir    = path.join(__dirname, '../res');
+var cmdName = 'test';
+var command = commands.test;
+var cwd = process.cwd();
+var resDir = path.join(__dirname, '../res');
 
 if (process.argv.length >= 3) {
-  cmdName = process.argv[2];
-  command = commands[cmdName];
+    cmdName = process.argv[2];
+    command = commands[cmdName];
 }
 
 if (!command) {
-  process.stderr.write('Savor: invalid command\n');
-  process.exit(1);
+    process.stderr.write('Savor: invalid command\n');
+    process.exit(1);
 }
 
 copyResourcesToTarget();
 
 command.forEach(function(subcommand) {
-  spawn(subcommand.bin, subcommand.args, { cwd: cwd, stdio: "inherit" });
+    spawn(subcommand.bin, subcommand.args, {
+        cwd: cwd,
+        stdio: "inherit"
+    });
 });
 
 cleanTargetResources();
@@ -33,26 +38,26 @@ cleanTargetResources();
 ///// HELPERS /////
 
 function copyResourcesToTarget() {
-  var allResources = resources[cmdName];
-  if (!allResources) {
-    return;
-  }
+    var allResources = resources[cmdName];
+    if (!allResources) {
+        return;
+    }
 
-  allResources.forEach(function(res){
-    var srcFile    = path.join(resDir, res);
-    var targetFile = path.join(cwd, res);
-    fs.copySync(srcFile, targetFile);
-  });
+    allResources.forEach(function(res) {
+        var srcFile = path.join(resDir, res);
+        var targetFile = path.join(cwd, res);
+        fs.copySync(srcFile, targetFile);
+    });
 }
 
 function cleanTargetResources() {
-  var allResources = resources[cmdName];
-  if (!allResources) {
-    return;
-  }
+    var allResources = resources[cmdName];
+    if (!allResources) {
+        return;
+    }
 
-  allResources.forEach(function(res){
-    var targetFile = path.join(cwd, res);
-    fs.removeSync(targetFile);
-  });
+    allResources.forEach(function(res) {
+        var targetFile = path.join(cwd, res);
+        fs.removeSync(targetFile);
+    });
 }
