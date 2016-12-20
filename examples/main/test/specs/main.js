4431cb357989d8001ab4be09eb4492ee5026d603
I. Dan Calinescu <idancalinescu@gmail.com>
2016-12-20 16:58:46 +0200

    added react native support

--- examples/main/test/specs/main.js
+++ examples/main/test/specs/main.js
@@ -1,9 +1,9 @@
-var savor = require('savor');
+var savor = require('../../../..');
 var main  = require('../../src/main');
 
 savor.add('should create a valid greeting', function(context, done) {
-  var greeting = main.createGreeting('Dan');
-  context.expect(greeting).to.equal("Hello, Dan");
+  var greeting = main.createGreeting('bob');
+  context.expect(greeting).to.equal("Hello, bob");
   done();
 }).
 
