40c28163c19eefb76187d14cb93c1d426ec0c35a
I. Dan Calinescu <idancalinescu@gmail.com>
2016-12-21 15:58:48 +0200

    made commands extensible

--- bin/commands.js
+++ bin/commands.js
@@ -1,13 +1,13 @@
 var testBin = "./node_modules/.bin/mocha";
-var coverageBin = "./node_modules/.bin/babel-node";
+var coverageBin = "./node_modules/.bin/istanbul";
 var lintBin = "./node_modules/.bin/eslint";
 var coverallsBin = "sh";
 var codacyBin = "sh";
 var codeclimateBin = "sh";
 var rmBin = "rm";
 var testArgs = ['--recursive', 'test/specs/**/*.js'];
-var coverageLocalArgs = ['./node_modules/.bin/istanbul', 'cover', './node_modules/.bin/_mocha', '--', '--report', 'lcovonly', 'test/specs/**/*.js'];
-var coverageArgs = ['./node_modules/.bin/istanbul', 'cover', './node_modules/.bin/_mocha', '--', '--report', 'lcovonly', 'test/specs/**/*.js'];
+var coverageLocalArgs = ['cover', './node_modules/.bin/_mocha', '--', '--report', 'lcovonly', 'test/specs/**/*.js'];
+var coverageArgs = ['cover', './node_modules/.bin/_mocha', '--', '--report', 'lcovonly', 'test/specs/**/*.js'];
 var coverallsArgs = ['-c', 'cat ./coverage/lcov.info | ./node_modules/.bin/coveralls'];
 var codacyArgs = ['-c', 'cat ./coverage/lcov.info | ./node_modules/.bin/codacy-coverage'];
 var codeclimateArgs = ['-c', './node_modules/.bin/codeclimate-test-reporter < ./coverage/lcov.info'];
